01fe16ad797a4ce42535eda3e97465f6b0c72398
Hubert Graczyk <hubert.graczyk@betasystems.com>
2025-03-26 19:08:11 +0100

    CC fixes

--- src/main/java/com/graczun/inflationCalculator/InflationCalculator.java
+++ src/main/java/com/graczun/inflationCalculator/InflationCalculator.java
@@ -3,7 +3,6 @@ package com.graczun.inflationCalculator;
 import com.graczun.inflationCalculator.provider.DataProvider;
 import lombok.RequiredArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.HashMap;
 import java.util.Map;
@@ -19,16 +18,14 @@ public class InflationCalculator implements AutoCloseable {
     }
 
     public Integer getCurrentValue(Integer value, YearMonth yearMonth) {
-        LocalDate date = LocalDate.now();
-        return (int) Math.floor(getInflationRateBetweenDates(yearMonth, YearMonth.of(date.getYear(), date.getMonthValue())) * value);
+        return (int) Math.floor(getInflationRateBetweenDates(yearMonth, YearMonth.now()) * value);
     }
 
     public Map<YearMonth, Integer> getInflationValueInDates(Integer value, YearMonth startYearMonth) {
         Map<YearMonth, Integer> result = new HashMap<>();
-        LocalDate date = LocalDate.now();
         double valueWithDigits = value.doubleValue();
-        YearMonth currentYearMonth = YearMonth.of(date.getYear(), date.getMonthValue());
-        Map<YearMonth, Double> inflationInYearMonth = dataProvider.getInflationRatesBetweenDates(startYearMonth, YearMonth.of(date.getYear(), date.getMonthValue()));
+        YearMonth currentYearMonth = YearMonth.now();
+        Map<YearMonth, Double> inflationInYearMonth = dataProvider.getInflationRatesBetweenDates(startYearMonth, currentYearMonth);
         while (!currentYearMonth.isBefore(startYearMonth)) {
             valueWithDigits = valueWithDigits * inflationInYearMonth.getOrDefault(startYearMonth, 1.0);
             result.put(startYearMonth, (int) Math.floor(valueWithDigits));
