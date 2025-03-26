445a2ac4259bbe98f9c870b253e9b31409590719
Hubert Graczyk <hubert.graczyk@betasystems.com>
2025-03-26 19:01:50 +0100

    New methods to get:  * Current value of amount in the past  * Inflated value for each month

--- src/main/java/com/graczun/inflationCalculator/InflationCalculator.java
+++ src/main/java/com/graczun/inflationCalculator/InflationCalculator.java
@@ -3,7 +3,9 @@ package com.graczun.inflationCalculator;
 import com.graczun.inflationCalculator.provider.DataProvider;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.YearMonth;
+import java.util.HashMap;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -13,7 +15,26 @@ public class InflationCalculator implements AutoCloseable {
 
     public double getInflationRateBetweenDates(YearMonth startYearMonth, YearMonth endYearMonth) {
         Map<YearMonth, Double> inflationInYearMonth = dataProvider.getInflationRatesBetweenDates(startYearMonth, endYearMonth);
-        return inflationInYearMonth.values().stream().reduce(1.0, (a, b) -> a * b) - 1.0;
+        return inflationInYearMonth.values().stream().reduce(1.0, (a, b) -> a * b);
+    }
+
+    public Integer getCurrentValue(Integer value, YearMonth yearMonth) {
+        LocalDate date = LocalDate.now();
+        return (int) Math.floor(getInflationRateBetweenDates(yearMonth, YearMonth.of(date.getYear(), date.getMonthValue())) * value);
+    }
+
+    public Map<YearMonth, Integer> getInflationValueInDates(Integer value, YearMonth startYearMonth) {
+        Map<YearMonth, Integer> result = new HashMap<>();
+        LocalDate date = LocalDate.now();
+        double valueWithDigits = value.doubleValue();
+        YearMonth currentYearMonth = YearMonth.of(date.getYear(), date.getMonthValue());
+        Map<YearMonth, Double> inflationInYearMonth = dataProvider.getInflationRatesBetweenDates(startYearMonth, YearMonth.of(date.getYear(), date.getMonthValue()));
+        while (!currentYearMonth.isBefore(startYearMonth)) {
+            valueWithDigits = valueWithDigits * inflationInYearMonth.getOrDefault(startYearMonth, 1.0);
+            result.put(startYearMonth, (int) Math.floor(valueWithDigits));
+            startYearMonth = startYearMonth.plusMonths(1);
+        }
+        return result;
     }
 
     @Override
